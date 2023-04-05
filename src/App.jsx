f53e370c74f71446f7787808d44721096f70331c
aaheli8 <sadhukhanaheli@gmail.com>
2023-04-05 13:21:42 +0530

    Restructuring Folders

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import Expenses from './components/Expenses'
+import Expenses from './components/EXPENSES/Expenses'
 
 function App() {
   const expenses = [
